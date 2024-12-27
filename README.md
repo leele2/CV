391e1a8e95e2ed18081c0935e0c872c9c6f6ed18
Lee <dj-luder@hotmail.com>
2024-12-27 16:34:51 +1100

    added cover letter with matching theme

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # CV
 
-![page0](z_output/Images/page0.png)
+![CVpage0](z_output/Images/CVpage0.png)
 ***
-![page1](z_output/Images/page1.png)
+![CVpage1](z_output/Images/CVpage1.png)
 ***
+
+
+# CoverLetterTemplate
+
+![CoverLetterTemplatepage0](z_output/Images/CoverLetterTemplatepage0.png)
+***
+
--- z_output/readme_gen.py
+++ z_output/readme_gen.py
@@ -5,31 +5,46 @@ from os.path import exists
 from os import mkdir
 import shutil
 
-# Directories
-cwd = str(Path(__file__).parent.absolute())
-pdf_file = cwd + "\\main.pdf"
-output_dir = cwd + "\\Images\\"
-readme_dir = "\\".join(cwd.split("\\")[:-1]) # -3 represents up 4 directories
- 
-# Store Pdf with convert_from_path function
-images = convert_from_path(pdf_file)
-txt_out = []
-# Add Title (root folder of repositry)
-txt_out.append("# " + cwd.split("\\")[-2] + "\n")
+def readme(pdf, text):
+    global readme_dir
+    # Directories
+    cwd = str(Path(__file__).parent.absolute())
+    pdf_file = cwd + f"\\{pdf}"
+    output_dir = cwd + "\\Images\\"
+    readme_dir = "\\".join(cwd.split("\\")[:-1]) # -3 represents up 4 directories
+    
+    # Store Pdf with convert_from_path function
+    images = convert_from_path(pdf_file)
+    txt_out = []
+    # Add Title (root folder of repositry)
+    # txt_out.append("# " + cwd.split("\\")[-2] + "\n")
+    txt_out.append("# " + text+ "\n")
 
-# Create folder to store images if not already created
-if not exists(output_dir):
-    mkdir(output_dir)
-# Convert pdf to images
-for i in range(len(images)):
-    #Save pdf pages as .png images
-    images[i].save(output_dir + 'page'+ str(i) +'.png', 'PNG')
-    #String for README.md
-    txt_out.append("![page" + str(i) + "](z_output/Images/page" + str(i) + ".png)")
-    # txt_out.append("![page" + str(i) + "](" + "/".join(output_dir.split("\\")[-5:]) + "page" + str(i) + ".png)")
-    txt_out.append("***")
+    # Create folder to store images if not already created
+    if not exists(output_dir):
+        mkdir(output_dir)
+    # Convert pdf to images
+    for i in range(len(images)):
+        #Save pdf pages as .png images
+        images[i].save(output_dir + f'{text}page'+ str(i) +'.png', 'PNG')
+        #String for README.md
+        txt_out.append(f"![{text}page" + str(i) + f"](z_output/Images/{text}page" + str(i) + ".png)")
+        # txt_out.append("![page" + str(i) + "](" + "/".join(output_dir.split("\\")[-5:]) + "page" + str(i) + ".png)")
+        txt_out.append("***")
+    # Copy pdf to main directory
+    shutil.copy(pdf_file, readme_dir + f"/{text}.pdf")
+    return txt_out
+
+pdf_files = [str(pdf_file).split('\\')[-1] for pdf_file in Path('./z_output/').glob("*.pdf")]
+txt_out = []
+for pdf_file in reversed(pdf_files):
+    result = readme(pdf_file, pdf_file.split('.')[0])
+    if "WorkingCoverLetter" in pdf_file:
+        # Dont add to ReadMe
+        continue
+    txt_out.extend(result)
+    txt_out.append("\n")
+
 # Create README.md
 with open(readme_dir + "/README.md", "w") as output:
     output.write("\n".join(txt_out))
-# Copy pdf to main directory
-shutil.copy(pdf_file, readme_dir + "/CV.pdf")
